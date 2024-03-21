5b701757e8fbf9f8a3ee11f7f88c359cb77a22c3
Luca Pontellini <luca.pontellini@studenti.isissgobetti.it>
2024-03-21 13:48:44 +0100

    edit

--- Numbered exercises/Pontellini_034.py
+++ Numbered exercises/Pontellini_034.py
@@ -14,8 +14,23 @@
 #Ricordarsi di etichettare l'asse x, l'asse y e di fornire un titolo per ogni grafico. Visualizza tutti i grafici alla fine.
 
 import argparse
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 #python plot_functions.py --linear --polinomial --exponential number    #script
+#Inserisci un numero per la variabile n e poi seleziona il tipo di funzione che vuoi graficare. Puoi scegliere tra ‘lineare’, 
+#‘polinomiale’ o ‘esponenziale’. A seconda della tua scelta, lo script creerà il grafico corrispondente. 
+#Ricorda che il grafico verrà salvato in un file alla fine.
 
-#def linear_equation ():
+def linear_equation (number: float, linear: str):list [float]
+
+number = float(input("Enter the number you want the linear equation for: '"))
+        #I valori x devono essere un elenco di valori compresi tra 0 e 10 (inclusi).
+        #I valori y devono essere calcolati in base al tipo di funzione e al numero di ingresso n.
+values_of_x = [0,1,2,3,4,5,6,7,8,9,10]
+values_of_y = []
+
+for numbers in range (values_of_x):
+    number * values_of_x
+    
+    return values_of_y
