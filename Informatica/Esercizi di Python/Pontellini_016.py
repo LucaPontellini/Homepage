bea87b01c52f4ae4d384e968cc6d62cc57672745
LucaPontellini <147493566+LucaPontellini@users.noreply.github.com>
2024-10-10 17:25:16 +0200

    edit

--- Informatica/Esercizi di Python/Pontellini_016.py
+++
@@ -1,8 +0,0 @@
-#Esercizio: Scrivi un programma che passati in input n valori popoli in una lista, la ristampi a video scorrendola con un for.
-
-list_of_peoples = []
-number_of_peoples = input ("Enter the number of value '")
-
-for peoples in number_of_peoples:
-    list_of_peoples.append (number_of_peoples)
-    print (f"The list contains {list_of_peoples} peoples")
