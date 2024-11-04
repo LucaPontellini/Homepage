d7d3b1b357c06f65b2ad3be2f50221b73edefb73
Luca Pontellini <luca.pontellini@studenti.isissgobetti.it>
2024-11-04 08:47:04 +0100

    edit

--- Informatica/Esercizi_di_Pyton_nuovi/test_01.py
+++ Informatica/Esercizi_di_Pyton_nuovi/test_01.py
@@ -1,9 +1,13 @@
 import pytest
 from Homepage.Informatica.Esercizi_di_Pyton_nuovi.Pontellini_001 import Person
 
-@pytest.fixture()
+@pytest.fixture(person=True)
+
+def test_creazione(self):
+    person1 = Person("Mario", 30, "Rome")
+
 def test_greets():
-    assert Person.greets()
+    assert Person.greets(self)
 
 def test_description():
-    assert Person.description()
+    assert Person.description(self)
