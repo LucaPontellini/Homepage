2933297750a3a3118f9b67481893739bdc0b1479
Luca Pontellini <luca.pontellini@studenti.isissgobetti.it>
2025-03-21 09:45:30 +0100

    edit

--- Computer_science/New_Python_exercises/Pontellini_032.py
+++ Computer_science/New_Python_exercises/Pontellini_032.py
@@ -15,11 +15,34 @@ class Platform:
         pass
 
 class Profile:
-    def __init__(self, name: str, email: str, password: str, subscription: float):
-        self.name = name
-        self.email = email
-        self.password = password
-        self.subscription = subscription
+    def __init__(self, name: str, email: str, password: str, subscription: Subscription, videos_viewed: list[Video]):
+        pass
+    def create_different_playlists(Playlist: list[str]):
+        pass
+    def add_videos_to_playlist(Playlist: playlist, Video: video):
+        pass
+    def remove_videos_from_playlist(Playlist: playlist, Video: video):
+        pass
+    def delete_playlist(Playlist: playlist):
+        pass
+    def leave_comments(Video: video, Comment: comment):
+        pass
+class Video:
+    def __init__(self, title: str, description: str, url: str, duration: datetime, views: int):
+        self.title = title
+        self.description = description
+        self.url = url
+        self.duration = duration
+        self.views = views
+    def add_comments(Comment: comment):
+        pass
 
-    def create_different_playlists(Playlist: list[str]): list[str]
-    def leave_comments(Video: video): str
+class Playlist:
+    def __init__(self, name: str, creator: str):
+        self.name = name
+        self.creator = creator
+
+    def add_videos(Video: video):
+        pass
+    def remove_videos(Video: video):
+        pass
