8c2e03eab48adfca94e5406178523ddfd043e46a
Luca Pontellini <luca.pontellini@studenti.isissgobetti.it>
2025-09-24 12:40:54 +0200

    edit

--- Computer_science/New_exercises_with_database/SQL/Pontellini_001.sql
+++ Computer_science/New_exercises_with_database/SQL/Pontellini_001.sql
@@ -94,19 +94,19 @@
 CREATE TABLE Beekeeper (
     Id_apicoltore INT PRIMARY KEY,
     Nome VARCHAR(50)
-    );
+);
 
 CREATE TABLE Tipology (
     Id_tipologia INT PRIMARY KEY,
     Nome_tipologia VARCHAR(50),
     Descrizione VARCHAR(255)
-    );
+);
 
 CREATE TABLE Honey (
     Id_miele INT PRIMARY KEY,
     Denominazione VARCHAR(50),
     Id_tipologia INT REFERENCES Tipology(Id_tipologia)
-    );
+);
 
 CREATE TABLE Apiary (
     Codice_apiario VARCHAR(10) PRIMARY KEY,
@@ -116,7 +116,7 @@ CREATE TABLE Apiary (
     Provincia VARCHAR(50),
     Regione VARCHAR(50),
     Id_apicoltore INT REFERENCES Beekeeper(Id_apicoltore)
-    );
+);
 
 CREATE TABLE Production (
     Id_produzione INT PRIMARY KEY,
@@ -124,22 +124,22 @@ CREATE TABLE Production (
     Quantita_annua FLOAT,
     Codice_apiario VARCHAR(10) REFERENCES Apiary(Codice_apiario),
     Id_miele INT REFERENCES Honey(Id_miele)
-    );
+);
     
--- Typology
-INSERT INTO Typology (id, typology_name, typology_description) VALUES
+-- Tipology
+INSERT INTO Tipology (Id_tipologia, Nome_tipologia, Descrizione) VALUES
 (1, 'Monofloral', 'Miele prodotto prevalentemente da un unico fiore'),
 (2, 'Polyfloral', 'Miele di millefiori, raccolto da più specie floreali'),
 (3, 'Honeydew', 'Miele prodotto a partire dal melato (secrezioni di insetti)');
 
 -- Beekeeper
-INSERT INTO Beekeeper (id, beekeeper_name) VALUES
+INSERT INTO Beekeeper (Id_apicoltore, Nome) VALUES
 (1, 'Marco Rossi'),
 (2, 'Lucia Bianchi'),
 (3, 'Alessandro Verdi');
 
 -- Honey
-INSERT INTO Honey (id, denomination, typology_id) VALUES
+INSERT INTO Honey (Id_miele, Denominazione, Id_tipologia) VALUES
 (1, 'Acacia', 1),
 (2, 'Castagno', 1),
 (3, 'Millefiori', 2),
@@ -147,28 +147,28 @@ INSERT INTO Honey (id, denomination, typology_id) VALUES
 (5, 'Melata di Bosco', 3);
 
 -- Apiary
-INSERT INTO Apiary (code, num_hives, locality, comune, province, region, beekeeper_id) VALUES
-(100, 12, 'Fattoria Le Rose', 'San Pietro', 'Pisa', 'Toscana', 1),
-(101, 8, 'Colle Verde', 'Montevarchi', 'Arezzo', 'Toscana', 2),
-(102, 20, 'Bosco Alto', 'Vercelli', 'Vercelli', 'Piemonte', 3),
-(103, 5, 'Terrazza Sud', 'Verona', 'Verona', 'Veneto', 1);
+INSERT INTO Apiary (Codice_apiario, Numero_arnie, Localita, Comune, Provincia, Regione, Id_apicoltore) VALUES
+('100', 12, 'Fattoria Le Rose', 'San Pietro', 'Pisa', 'Toscana', 1),
+('101', 8, 'Colle Verde', 'Montevarchi', 'Arezzo', 'Toscana', 2),
+('102', 20, 'Bosco Alto', 'Vercelli', 'Vercelli', 'Piemonte', 3),
+('103', 5, 'Terrazza Sud', 'Verona', 'Verona', 'Veneto', 1);
 
 -- Production
-INSERT INTO Production (id, year, quantity, apiary_code, honey_id) VALUES
-(1, 2022, 120.5, 100, 1),
-(2, 2022, 95.2, 101, 3),
-(3, 2023, 210.0, 102, 5),
-(4, 2023, 34.7, 103, 2),
-(5, 2024, 150.0, 100, 3),
-(6, 2024, 78.3, 101, 4);
+INSERT INTO Production (Id_produzione, Anno, Quantita_annua, Codice_apiario, Id_miele) VALUES
+(1, 2022, 120.5, '100', 1),
+(2, 2022, 95.2, '101', 3),
+(3, 2023, 210.0, '102', 5),
+(4, 2023, 34.7, '103', 2),
+(5, 2024, 150.0, '100', 3),
+(6, 2024, 78.3, '101', 4);
 
 SELECT * FROM Beekeeper;
 SELECT Nome FROM Beekeeper WHERE Id_apicoltore = 1;
-SELECT * FROM Apiary WHERE Regione = 'Lombardia';
+SELECT * FROM Apiary WHERE Regione = 'Lombardia'; -- Nessun risultato previsto
 SELECT Codice_apiario, Numero_arnie FROM Apiary WHERE Numero_arnie > 10;
 SELECT Codice_apiario, Localita FROM Apiary WHERE Id_apicoltore = 2;
 SELECT * FROM Honey WHERE Id_tipologia = 3;
 SELECT Denominazione FROM Honey WHERE Id_miele = 5;
 SELECT * FROM Production WHERE Anno = 2024;
-SELECT * FROM Production WHERE Codice_apiario = 'A001';
-SELECT * FROM Production WHERE Id_miele = 3 AND Anno = 2023;
+SELECT * FROM Production WHERE Codice_apiario = 'A001'; -- Nessun risultato previsto
+SELECT * FROM Production WHERE Id_miele = 3 AND Anno = 2023; -- Nessun risultato previsto
